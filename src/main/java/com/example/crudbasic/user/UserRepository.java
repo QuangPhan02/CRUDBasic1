35bcad681ce515fd46d3cacc02103dc0581ab592
QuangPhannn <duckson02@gmail.com>
2022-04-21 08:26:33 +0700

    code CRUD hoàn thiện + chức năng tìm kiếm mới

--- src/main/java/com/example/crudbasic/user/UserRepository.java
+++ src/main/java/com/example/crudbasic/user/UserRepository.java
@@ -2,5 +2,10 @@ package com.example.crudbasic.user;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
+
 public interface UserRepository extends CrudRepository<User, Integer> {
+    public Long countById(Integer id);
+    List<User> findByLastNameNameLikeOrderByLastName(String lastName);
 }
